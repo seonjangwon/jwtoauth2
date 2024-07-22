d4ca93e833370a7d52d31e035777400ad65844d4
seonjangwon <wkddnjs740@naver.com>
2024-07-22 14:26:19 +0900

    Initial commit

---
+++ src/main/java/com/security/jwtoauth2/Jwtoauth2Application.java
@@ -0,0 +1,13 @@
+package com.security.jwtoauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jwtoauth2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jwtoauth2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/security/jwtoauth2/Jwtoauth2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.security.jwtoauth2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jwtoauth2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
